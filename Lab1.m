ece92ccacbe02da4e4d40a447691f0e462d4030f
BrianAmin <tuf49787@temple.edu>
2019-01-15 16:39:39 -0500

    Now just need to estimate probability of ORF nonstop codon length

--- Lab1.m
+++ Lab1.m
@@ -1,10 +1,16 @@
 %% Part 1.1.1 Download sequence with accession number and load into MATLAB
 % Retrieve sequence, save to file
 clc;close all; clear;
-getgenbank('nm_000520', 'toFile', 'nm_000520.txt');
 %%
-% Bring file into matlab
-s = genbankread('nm_000520.txt');
+% Check if file already exists, if it doesn't. Download file from genbank
+if exist('nm_000520.txt') == 2
+    % Bring file into matlab
+    s = genbankread('nm_000520.txt');
+else
+    % Grab file and then bring it into matlab
+    getgenbank('nm_000520', 'toFile', 'nm_000520.txt');
+    s = genbankread('nm_000520.txt');
+end
 %%
 % Extract sequence
 seq = getgenbank('nm_000520', 'SequenceOnly', true);
@@ -46,9 +52,23 @@ seq_density_n30 = ntdensity(s.Sequence, 'WINDOW',round(length(s.Sequence)/30))
 % allow for one to see large amounts of data within one space
 
 %% Part 1.3.1: Condon Count and ORF
-% Return codon counts for the first reading frame
+% Return codon counts for each of the six reading frames
 % Plot the results in a heat map
-rlcodons = codoncount(s.Sequence, 'frame', 1, 'figure', true);
-
+figure(5)
+rlcodons_1 = codoncount(s.Sequence, 'frame', 1, 'figure', true);
+title('Frame 1')
+figure(6)
+rlcodons_2 = codoncount(s.Sequence, 'frame', 2, 'figure', true);
+title('Frame 2')
+figure(7)
+rlcodons = codoncount(s.Sequence, 'frame', 3, 'figure', true);
+title('Frame 3')
 %% Part 1.3.1: Find ORFs based on length
 
+%%
+% Find ORFS of length > 50 in Frame 1 of the given sequence
+orf_min_50 = seqshoworfs(s.Sequence, 'Frames', 1, 'MinimumLength', 50);
+% Find ORFS of length > 500 in Frame 1 of the given sequence
+orf_min_500 = seqshoworfs(s.Sequence, 'Frames', 1, 'MinimumLength', 500);
+
+%% Part 1.3.2 Finding optimum threshold for ORFs

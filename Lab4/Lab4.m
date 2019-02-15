86897ad4ef72564de91eb81f9994d043e75116e0
BrianAmin <tuf49787@temple.edu>
2019-02-15 00:55:40 -0500

    Finished Lab 4, able to sequence DNA into segments and through parallel computing toolbox

--- Lab4/Lab4.m
+++ Lab4/Lab4.m
@@ -1,4 +1,5 @@
-
+%% ECES T580: Lab 4
+% Bhautik (Brian) Amin
 
 %% Lab 3.1.1
 
@@ -45,11 +46,11 @@ title('NonParallel')
 %%
 % 3. Use the new code to parallelize the previous job
 
-delete(gcp('nocreate')) % Clear previous workers and set up new pool
+% delete(gcp('nocreate')) % Clear previous workers and set up new pool
 
 stft_position = {}; % Initialize cell array to score processed chunks (Will be out of order)
 worker_num = 2; % Setup for 2 core computer
-parpool(worker_num); % Setup worker pool
+%parpool(worker_num); % Setup worker pool
 disp('Parallelized method')
 tic % Begin timer
 parfor i=1:worker_num
@@ -62,19 +63,59 @@ for i=1:worker_num
     ordered_data = [ordered_data, stft_position(i)];
 end
 toc
-%%
+%% Lab 3.2.1
 % Plot results for comparision
 ordered_data = cell2mat(ordered_data); % Convert to struct
 figure(2)
 plot(ordered_data)
+title('Parallel Method')
 
-
+%%
 % check difference:
 diff = sum(abs(ordered_data - Threebaseperiodicity_vs_position_old));
 disp('Absolute Sum Error:')
 disp(diff)
 
+%%
+% Plot the results on top of each other
+figure(3)
+plot(Threebaseperiodicity_vs_position_old)
+hold on;
+plot(ordered_data)
+title('Side by Side Comparison Parallel and Non-Parallel')
 
 
+%% Function Appendix
+function Threebaseperiodicity_vs_position = parallel_threebasefreq_stft (DNA_SEQUENCE, WINDOW_LENGTH, NFFT, division, index)
+% Function Goals:
+% Divide DNA into a sequence of parts. For each parallelized iteration, the
+% function needs to know which part is which and figure out which
+% subsequence it will need to extract (start and end indices). Make sure
+% all possible window positions are covered by the subsequence
+
+
+% Calculate the amount of chunks desired (Depends on number of available
+% workers)
+chunks_size = floor(length(DNA_SEQUENCE)/division); %Floor used to round down number
+
+
+
+% Determine start point based on index
+start_point = (index-1)*chunks_size+1;
+
+
+% If the index is the last segment of DNA, otherwise seg the end point with
+% respect to the index and desired chunk size
+if index == division
+    end_point = length(DNA_SEQUENCE);
+else
+    end_point = index*chunks_size+WINDOW_LENGTH-1;
+end
+
+% Extract desired DNA sequence now
+seq_div = DNA_SEQUENCE(start_point:end_point);
+% Run the individual segment through threebasefreq_stft and return results
+Threebaseperiodicity_vs_position = threebasefreq_stft (seq_div, WINDOW_LENGTH, NFFT);
+end
 
 
--- Lab4/parallel_threebasefreq_stft.m
+++ Lab4/parallel_threebasefreq_stft.m
@@ -16,7 +16,7 @@ chunks_size = floor(length(DNA_SEQUENCE)/division); %Floor used to round down nu
 
 %%
 % Determine start point based on index
-start_point = (chunks_size+1) * (index-1);
+start_point = (index-1)*chunks_size+1;
 
 %%
 % If the index is the last segment of DNA, otherwise seg the end point with
@@ -28,7 +28,7 @@ else
 end
 %%
 % Extract desired DNA sequence now
-seq_div = DNA_SEQUENCE(start_point:end_point
+seq_div = DNA_SEQUENCE(start_point:end_point);
 % Run the individual segment through threebasefreq_stft and return results
 Threebaseperiodicity_vs_position = threebasefreq_stft (seq_div, WINDOW_LENGTH, NFFT);
 
